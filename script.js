1f990fc78620d0c3cda9ef07018fb3c01c2f597d
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-03-06 15:40:58 -0800

    Add function to control snake

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvasBackgroundColour = "white";
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d");
+document.addEventListener("keydown", changeDirection);
 
 //  Select the colour to fill the drawing
 ctx.fillStyle = canvasBackgroundColour;
@@ -16,6 +17,11 @@ let snake = [
     { x: 120, y: 150 },
     { x: 110, y: 150 },
 ];
+
+let dx = 10;
+let dy = 0;
+
+main();
 
 
 function main() {
@@ -40,14 +46,28 @@ function drawSnakePart(snakePart) {
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
+function changeDirection(event) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+    if (keyPressed === LEFT_KEY && !goingRight) { dx = -10; dy = 0; }
+    if (keyPressed === UP_KEY && !goingDown) { dx = 0; dy = -10; }
+    if (keyPressed === RIGHT_KEY && !goingLeft) { dx = 10; dy = 0; }
+    if (keyPressed === DOWN_KEY && !goingDown) { dx = 0; dy = 10; }
+}
+
 function drawSnake() {
     snake.forEach(drawSnakePart);
 }
 
 function advanceSnake() {
-    const head = { x: snake[0].x + 10, y: snake[0].y + 0 };
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
     snake.unshift(head);
     snake.pop();
 }
-
-main();
