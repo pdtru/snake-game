63cd7df55c9c13603e2c45152114fa2853b7faca
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-03-06 15:31:41 -0800

    Add main function to move snake

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
 const canvasBackgroundColour = "white";
-
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d");
 
- //  Select the colour to fill the drawing
- ctx.fillStyle = canvasBackgroundColour;
+//  Select the colour to fill the drawing
+ctx.fillStyle = canvasBackgroundColour;
+// Draw a "filled" rectangle to cover the entire canvas
+ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+// Draw a "border" around the entire canvas
+ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 
- // Draw a "filled" rectangle to cover the entire canvas
- ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
- // Draw a "border" around the entire canvas
- ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+let snake = [
+    { x: 150, y: 150 },
+    { x: 140, y: 150 },
+    { x: 130, y: 150 },
+    { x: 120, y: 150 },
+    { x: 110, y: 150 },
+];
+
+
+function main() {
+    setTimeout(function onTick() {
+        clearCanvas();
+        advanceSnake();
+        drawSnake();
+        main();
+    }, 100)
+}
+function clearCanvas() {
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
+
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokestyle = 'darkgreen';
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+function advanceSnake() {
+    const head = { x: snake[0].x + 10, y: snake[0].y + 0 };
+    snake.unshift(head);
+    snake.pop();
+}
+
+main();
